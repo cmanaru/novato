995efc0674d4fac815b70512f8e2c6cbb2a6a890
Ernesto Anaya <ernestex@gmail.com>
2013-09-13 11:35:51 -0500

    estructura for

--- ejercicio1.php
+++ ejercicio1.php
@@ -12,5 +12,18 @@ if ($c < $b) {
         echo " $c es menor a $b ";
 }
 
+if ( ($c < $b) || ($a > 25) ) {
+        echo " *** ";
+}
+
+
+ 
+for( $i=0; $i<10 ; $i=$i+1 ){
+    
+   echo "Una linea: ";
+   echo "valor de i = $i \n"; 
+   
+}
+
 
 
