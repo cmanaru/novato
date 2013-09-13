6067127fe9676acc5346974888d9ada45728d502
Chrystian Anaya <cmanaru@gmail.com>
2013-09-13 11:27:23 -0500

    ejemplo if

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,13 +1,16 @@
 <?php 
 
-$a = 3;
+$a = 30;
 $b = 7;
+$c = 2;
 
-
-if ($a > $b){
-	echo " $a es mayor a $b ";
+if ($a > $b) {
+	echo " $a es mayor a $b \n";
 }
 
+if ($c < $b) {
+        echo " $c es menor a $b ";
+}
 
 
 
